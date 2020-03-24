6f5cd82bcc94f27a7316e72c8eeabd834254ee4d
santmendoza <47405047+santmendoza@users.noreply.github.com>
2020-03-24 18:27:27 -0500

    Prueba2

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 <p>My first paragraph.</p>
 
   
-  <iframe height="400px" width="100%" src="https://repl.it/repls/ScholarlyRudeInverse?lite=true" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
+  <iframe height="600px" width="100%" src="https://repl.it/repls/ScholarlyRudeInverse?lite=true" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
 </body>
 </html>
